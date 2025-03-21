46188885a51d4b54862bfea57c2f606100c36f1c
yardkeeper <46852397+yardkeeper@users.noreply.github.com>
2025-03-21 12:30:04 +0200

    Add files via upload

--- jetpack_install.sh
+++ jetpack_install.sh
@@ -8,6 +8,40 @@ read  REMOTE_USER
 echo -n "Enter SSH password for $REMOTE_USER@$REMOTE_HOST: "
 read -s REMOTE_PASSWORD 
 
+check_packages_installed() {
+    for PACKAGE in "$@"; do
+        dpkg -l | grep -q "^ii  $PACKAGE"
+        if [ $? -eq 0 ]; then
+            echo "Package '$PACKAGE' is installed."
+        else
+            echo "Package '$PACKAGE' is not installed."
+        fi
+    done
+}
+
+rootfs(){
+   tar -xzvpf rootfs.tar.gz
+}
+
+rootfs_cleanup(){
+  if [ -d "$folder" ];
+  then
+    rm -rf $CURRENT_LOCATION/rootfs
+  else
+    sleep 1s
+  fi
+}
+
+generate_random_id() {
+  openssl rand -base64 7 | tr -dc 'A-Za-z0-9' | head -c 10
+}
+
+function create_default_user(){
+ RAND_ID=$(generate_random_id)
+ bash  tools/l4t_create_default_user.sh -u $REMOTE_USER -p $REMOTE_PASSWORD -n orin-$RAND_ID  --accept-license
+}
+   
+
 function flash(){
     bash flash.sh "$1" mmcblk0p1
     if [ $? -eq 1 ]; then
@@ -37,27 +71,38 @@ export -f ssh_check
 
 
 function ssh_copy() {
-    for dir in debs deps; 
+    echo "Copying files..."
+    for dir in debs; 
     do
         sshpass -p "$REMOTE_PASSWORD" scp -o StrictHostKeyChecking=no -r "$CURRENT_LOCATION/$dir" "$REMOTE_USER@$REMOTE_HOST:/home/$REMOTE_USER"
     done
 }
 
 function install_orin(){
+check_packages_installed "sshpass" "qemu-user-static"
+rootfs_cleanup
+rootfs
+create_default_user
 flash "jetson-agx-orin-devkit"
 ssh_check
 ssh_copy
 
+#SRC_DIR="./aksusbd-9.13.1"
+
 sshpass -p "$REMOTE_PASSWORD" ssh -T $REMOTE_USER@$REMOTE_HOST << EOF
   echo "$REMOTE_PASSWORD" | sudo -S dpkg -i /home/$REMOTE_USER/debs/*
-  echo "$REMOTE_PASSWORD" | sudo -S tar -xzf /home/$REMOTE_USER/deps/OpenCV-4.5.0-aarch64-Orin-JetPack-5.1.2.tar.gz --strip-components=1 -C /usr/local/
+  #echo "$REMOTE_PASSWORD" | sudo -S tar -xzf /home/$REMOTE_USER/deps/OpenCV-4.5.0-aarch64-Orin-JetPack-5.1.2.tar.gz --strip-components=1 -C /usr/local/
   echo "$REMOTE_PASSWORD" | sudo -S ldconfig
-  echo "$REMOTE_PASSWORD" | sudo -S mkdir -p /mnt/videos
-  echo "$REMOTE_PASSWORD" | sudo -S chown -R nobody:nogroup /mnt/videos/
-  echo "$REMOTE_PASSWORD" | sudo -S chmod 777 /mnt/videos/
+  echo "$REMOTE_PASSWORD" | sudo -S mkdir -p /mnt/foresight_videos
+  echo "$REMOTE_PASSWORD" | sudo -S chown -R nobody:nogroup /mnt/foresight_videos/
+  echo "$REMOTE_PASSWORD" | sudo -S chmod 777 /mnt/foresight_videos/
   echo "/mnt/  192.168.1.0/24(fsid=1001,rw,sync,no_subtree_check)" | sudo tee -a /etc/exports > /dev/null
+ # echo "$REMOTE_PASSWORD" | sudo -S tar -xzvf /home/$REMOTE_USER/deps/aksusbd_33876-9.13.1_arm64_and_amd64.tar.gz
+ # echo "$REMOTE_PASSWORD" | sudo -S ./aksusbd-9.13.1/dinst "$SRC_DIR"
+  
 EOF
 
+ 
   
 }
 
@@ -68,6 +113,7 @@ while true; do
     echo "Installation options:"
     echo ""
     echo "1 - Install Jetpack 5.1.2 for Nvidia Orin "
+    
     read choice
     
     case "$choice" in
@@ -76,6 +122,8 @@ while true; do
             install_orin
             break
         ;;
+
+     
      
          r)
              remove
