e7f1d89fe31b66212b91b31e09443a5fe276f425
yardkeeper <46852397+yardkeeper@users.noreply.github.com>
2025-03-26 17:12:29 +0200

    Update jetpack_install.sh

--- jetpack_install.sh
+++ jetpack_install.sh
@@ -97,11 +97,6 @@ function ssh_install(){
 
 sshpass -p "$REMOTE_PASSWORD" ssh -T $REMOTE_USER@$REMOTE_HOST << EOF
   echo "$REMOTE_PASSWORD" | sudo -S dpkg -i /home/$REMOTE_USER/debs/*
-  echo "$REMOTE_PASSWORD" | sudo -S ldconfig
-  echo "$REMOTE_PASSWORD" | sudo -S mkdir -p /mnt/foresight_videos
-  echo "$REMOTE_PASSWORD" | sudo -S chown -R nobody:nogroup /mnt/foresight_videos/
-  echo "$REMOTE_PASSWORD" | sudo -S chmod 777 /mnt/foresight_videos/
-  echo "/mnt/  192.168.1.0/24(fsid=1001,rw,sync,no_subtree_check)" | sudo tee -a /etc/exports > /dev/null
 
 EOF
 
